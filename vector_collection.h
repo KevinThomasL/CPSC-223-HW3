5a09c9fe9f0ec0571f53be91ddd163f280b39d50
Shawn Bowers <smbowers@gmail.com>
2020-02-03 16:28:53 -0800

    Initial commit

---
+++ vector_collection.h
@@ -0,0 +1,52 @@
+//----------------------------------------------------------------------
+// Author:
+// Course: CPSC 223, Spring 2020
+// Assign: 3
+// File:   vector_collection.h
+//
+// TODO: Description here
+//----------------------------------------------------------------------
+
+#ifndef VECTOR_COLLECTION_H
+#define VECTOR_COLLECTION_H
+
+#include <vector>
+#include <algorithm>            
+#include "collection.h"
+
+
+template<typename K, typename V>
+class VectorCollection : public Collection<K,V>
+{
+public:
+
+  // add a new key-value pair into the collection 
+  void add(const K& a_key, const V& a_val);
+
+  // remove a key-value pair from the collectiona
+  void remove(const K& a_key);
+
+  // find and return the value associated with the key
+  bool find(const K& search_key, V& the_val) const;
+
+  // find and return the values with keys >= to k1 and <= to k2 
+  void find(const K& k1, const K& k2, std::vector<V>& vals) const;
+  
+  // return all of the keys in the collection 
+  void keys(std::vector<K>& all_keys) const;
+
+  // return all of the keys in ascending (sorted) order
+  void sort(std::vector<K>& all_keys_sorted) const;
+
+  // return the number of key-value pairs in the collection
+  int size() const;
+
+private:
+  std::vector<std::pair<K,V>> kv_list;
+};
+
+
+// TODO: Implement member functions here ...
+
+
+#endif
